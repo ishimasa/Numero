23b8830ada66a053d10322f249ac96b611e45eaa
M.Ishimoto <ishitra777@gmail.com>
2018-06-26 09:19:45 +0900

    Initial Commit

---
+++ iNumero/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  iNumero
+//
+//  Created by eagle two on 2018/06/26.
+//  Copyright © 2018年 isshie. All rights reserved.
+//
+        
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
